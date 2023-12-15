27d53996e81d96983706099ca12290774e8256ff
stepan-izvolskii <izvolstep@gmail.com>
2023-12-15 17:54:16 +0300

    Add the 'app/__init__.py' file
    It includes the runtime data and the SECRET_KEY

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
+
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 USERS = []  # a list of User-type objects
 CONTESTS = []   # a list of Contest-type objects
+ENDPOINT = 'http://127.0.0.1:5000'
 
-from app import views_all
-from app import models
-from app import views
+from app import forms
 from app import tests
+from app import views
+from app import models
+from app import views_all
