3a3bd9e900a9d85758902c2b183f2c93275d65ca
stepan-izvolskii <izvolstep@gmail.com>
2023-12-15 16:51:16 +0300

    feat: Add a function get_valid_users
    get_valid_users returns the list of the users which the status "Created",
    i.e to provide some other functions with this list as a valid input

--- app/funcs.py
+++ app/funcs.py
@@ -1,30 +1,35 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from app import USERS
+from app import models, USERS
 
 
-def get_users_leaderboard(sort):
+def get_valid_users(USERS=USERS):
+    # returns the list of all USERS that are NOT DELETED
+    users_valid = [user for user in USERS if models.User.is_valid_id(user.id)]
+    return users_valid
+
+
+def get_users_leaderboard(sort = 'desc'):
     # creates an array of users' leaderboard
-    users_actual = [user for user in USERS if user.is_valid_id(user.id)]
+    users_list = get_valid_users()
     if sort == "asc":
-        users_actual.sort(key=lambda user: len(user.contests))
+        users_list.sort(key=lambda user: len(user.contests))
     else:
-        users_actual.sort(key=lambda user: len(user.contests), reverse=True)
-    return users_actual
+        users_list.sort(key=lambda user: len(user.contests), reverse=True)
+    return users_list
 
 
 def create_graph():
     # creates a graph of users' leaderboard
-    users_actual = [user for user in USERS if user.is_valid_id(user.id)]
+    users_list = get_valid_users()
     x = np.array(
-        [f"{user.id}, {user.last_name}" for user in users_actual])
-    y = np.array([len(user.contests) for user in users_actual])
+        [f"{user.id}# {user.last_name}" for user in users_list])
+    y = np.array([len(user.contests) for user in users_list])
     plt.figure()
-    plt.xticks(ticks=np.array([user.id for user in users_actual]), rotation=20)
-    plt.yticks(ticks=np.arange(min(y), max(y)))
-    plt.title("The leaderboard of all users by numbers of contests")
+    plt.xticks(ticks=np.array([users_list.index(user) for user in users_list]), rotation=10)
+    plt.yticks(ticks=np.arange(min(y), max(y)+1))
     plt.xlabel("users' ids and last names")
     plt.ylabel("number of contests")
     plt.grid()
     plt.bar(x, y, width=0.1)
-    plt.savefig("app/leaderboard.png")
+    plt.savefig("app/static/leaderboard.png")
