dae4cf91cfe7d44b441c034564a9c89a885a6e3a
stepan-izvolskii <izvolstep@gmail.com>
2023-12-15 17:41:13 +0300

    Add 'views_all.py'
    This file 'render_template' of the index

--- app/views_all.py
+++ app/views_all.py
@@ -1,14 +1,7 @@
 from app import app, USERS, CONTESTS
-from flask import Response
-from http import HTTPStatus
+from flask import render_template
 
 
 @app.route("/")
 def index():
-    # the homepage, where the data of all stored objects is represented
-    response = (
-        f"<h2>Hello,world!<h2>"
-        f"<br>USERS:<br>{'<br>'.join([user.repr() for user in USERS])}<br>"
-        f"<br>CONTESTS:<br>{'<br>'.join([contest.repr() for contest in CONTESTS])}"
-    )
-    return Response(response, HTTPStatus.OK)
+    return render_template('index.html', USERS=USERS, CONTESTS=CONTESTS)
